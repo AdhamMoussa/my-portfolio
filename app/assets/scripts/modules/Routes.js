e905dab6b57c4a85b9386a3e06b4bcd8c8d40b0b
Adham Moussa <adam.moussa62017@gmail.com>
2018-08-11 07:12:56 +0200

    demo build

--- app/assets/scripts/modules/Routes.js
+++ app/assets/scripts/modules/Routes.js
@@ -59,15 +59,15 @@ export default class Routes {
     router
       .on({
         "": function() {
-          that.routesHandler("/home.html", that.homeLink);
+          that.routesHandler("/portfolio/home.html", that.homeLink);
           console.log("home routing");
         },
         "/portfolio": function() {
-          that.routesHandler("/portfolio.html", that.portfolioLink);
+          that.routesHandler("portfolio/portfolio.html", that.portfolioLink);
           console.log("portf routing");
         },
         "/contact": function() {
-          that.routesHandler("/contact.html", that.contactLink);
+          that.routesHandler("portfolio/contact.html", that.contactLink);
         }
       })
       .resolve();
